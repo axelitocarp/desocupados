de6f7cc2d6dff1a9790173b192ff8beedac98982
thevegit0 <taieltraversa@gmail.com>
2017-09-08 09:32:10 -0300

    desocupados models maty

--- colocaciones/models.py
+++ colocaciones/models.py
@@ -7,3 +7,38 @@ class Agencia(models.Model):
     def __str__(self):
             return self.title
 
+
+
+class Persona(models.Model):
+    dni = models.CharField(max_length=8)
+    tipoDeTrabajoQuePuedeRealizar
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    fechaDeNcaimiento
+    desocupado
+
+class Empleo(models.Model):
+    persona
+    oferta
+    empresa
+    inicioDeContrato
+    finDeContrato
+
+class Rubro(models.Model):
+    tipoDeTrabajo
+
+class Oferta(models.Model):
+    empresa
+    necesidad
+    fecha
+    activa
+    inicioContrato
+    finContrato
+
+class Empresa(models.Model):
+    cuit
+    razonSocial
+    rubro
+    listaDeOfertas
+    activa
+
